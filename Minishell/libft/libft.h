34e5d5af1a44f7a8fe1edce11e68c7f80290194d
lpiccirilli <119267142+lpiccirilli@users.noreply.github.com>
2024-02-15 19:45:38 +0100

    start

---
+++ Minishell/libft/libft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpicciri <lpicciri@student.42roma.it>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/26 18:08:42 by lpicciri          #+#    #+#             */
+/*   Updated: 2024/01/25 16:06:58 by lpicciri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <unistd.h>
+# include <stdlib.h>
+
+
+size_t	ft_strlen(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+#endif
