0a05b7c6ef70480e5f3d873089ad45fdb24f48d6
mcamilli <mcamilli@student.42roma.it>
2024-02-15 19:38:59 +0100

    first push

---
+++ mini/minishell.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcamilli <mcamilli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/14 14:24:09 by mcamilli          #+#    #+#             */
+/*   Updated: 2024/02/14 14:27:48 by mcamilli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif
